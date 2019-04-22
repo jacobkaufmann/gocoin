0d7b0e4c1545e4191e7e7173898421d8c014154d
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-04-21 18:43:27 -0700

    Initial commit for hash utilities.

---
+++ pkg/crypto/hash.go
@@ -0,0 +1,53 @@
+package crypto
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+// HashSize of array used to store hashes.  See Hash.
+const HashSize = 32
+
+// Hash is used in several of the bitcoin messages and common structures.  It
+// typically represents the double sha256 of data.
+type Hash [HashSize]byte
+
+// MaxHashStringSize is the maximum length of a Hash hash string.
+const MaxHashStringSize = HashSize * 2
+
+// ErrHashStrSize describes an error that indicates the caller specified a hash
+// string that has too many characters.
+var ErrHashStrSize = fmt.Errorf("max hash string length is %v bytes", MaxHashStringSize)
+
+// String returns the Hash as the hexadecimal string of the byte-reversed
+// hash.
+func (hash Hash) String() string {
+	for i := 0; i < HashSize/2; i++ {
+		hash[i], hash[HashSize-1-i] = hash[HashSize-1-i], hash[i]
+	}
+	return hex.EncodeToString(hash[:])
+}
+
+// SetBytes sets the bytes which represent the hash.  An error is returned if
+// the number of bytes passed in is not HashSize.
+func (hash *Hash) SetBytes(newHash []byte) error {
+	nhlen := len(newHash)
+	if nhlen != HashSize {
+		return fmt.Errorf("invalid hash length of %v, want %v", nhlen,
+			HashSize)
+	}
+	copy(hash[:], newHash)
+
+	return nil
+}
+
+// NewHash returns a new Hash from a byte slice.  An error is returned if
+// the number of bytes passed in is not HashSize.
+func NewHash(newHash []byte) (*Hash, error) {
+	var hash Hash
+	err := hash.SetBytes(newHash)
+	if err != nil {
+		return nil, err
+	}
+	return &hash, err
+}
---
+++ pkg/crypto/hashfuncs.go
@@ -0,0 +1,41 @@
+package crypto
+
+import (
+	"crypto/sha256"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+// SHA256B performs the SHA-256 hashing algorithm and returns the corresponding
+// bytes.
+func SHA256B(b []byte) []byte {
+	hash := sha256.Sum256(b)
+	return hash[:]
+}
+
+// SHA256H performs the SHA-256 hashing algorithm and returns the corresponding
+// Hash.
+func SHA256H(b []byte) Hash {
+	return Hash(sha256.Sum256(b))
+}
+
+// DoubleSHA256B performs the SHA-256 hashing algorithm twice sequentially and
+// returns the corresponding bytes.
+func DoubleSHA256B(b []byte) []byte {
+	hash := SHA256B(SHA256B(b))
+	return hash[:]
+}
+
+// DoubleSHA256H performs the SHA-256 hashing algorithm twice sequentially and
+// returns a Hash.
+func DoubleSHA256H(b []byte) Hash {
+	return SHA256H(SHA256B(b))
+}
+
+// Hash160 performs the Hash160 hashing algorithm and returns the corresonding
+// bytes.
+func Hash160(b []byte) []byte {
+	hash := SHA256B(b)
+	hash = ripemd160.New().Sum(hash)
+	return hash
+}
