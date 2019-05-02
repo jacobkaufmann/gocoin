120e279316a966a2a66d932ce0b324845a92ffc0
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-05-01 21:05:47 -0700

    Refactor as wrapper around ecdsa.PublicKey.

--- pkg/crypto/btcec/pubkey.go
+++ pkg/crypto/btcec/pubkey.go
@@ -2,56 +2,90 @@ package btcec
 
 import (
 	"crypto/ecdsa"
-	"encoding/hex"
 	"math/big"
 
 	"github.com/jacobkaufmann/gocoin/pkg/crypto/hashing"
-
 	"github.com/jacobkaufmann/gocoin/pkg/util/encoding/base58"
 )
 
-// A PublicKey represents a Bitcoin public key.
-type PublicKey struct {
-	*ecdsa.PublicKey
-	compressed bool
+// These constants define the lengths of serialized public keys.
+const (
+	PubKeyBytesLenCompressed   = 33
+	PubKeyBytesLenUncompressed = 65
+)
+
+// These constants define the prefixes for serialized public keys.
+const (
+	pubkeyCompressedEvenY byte = 0x2
+	pubKeyCompressedOddY  byte = 0x3
+	pubkeyUncompressed    byte = 0x4
+)
+
+func isOdd(a *big.Int) bool {
+	return a.Bit(0) == 1
 }
 
-// NewPublicKey returns the public key corresponding to a PrivateKey.
-func NewPublicKey(privKey *PrivateKey) *PublicKey {
-	pubKey := &PublicKey{
-		&privKey.PublicKey,
-		privKey.compressed,
-	}
-	return pubKey
-}
-
-// Compressed returns whether or not the PublicKey is compressed.
-func (k *PublicKey) Compressed() bool {
-	return k.compressed
+// IsCompressedPubKey returns true the the passed serialized public key has
+// been encoded in compressed format, and false otherwise.
+func IsCompressedPubKey(pubKey []byte) bool {
+	// The public key is only compressed if it is the correct length and
+	// the format (first byte) is one of the compressed pubkey values.
+	return len(pubKey) == PubKeyBytesLenCompressed &&
+		(pubKey[0]&^byte(0x1) == pubkeyCompressedEvenY ||
+			pubKey[0]&^byte(0x1) == pubKeyCompressedOddY)
 }
 
 // Address returns the Bitcoin address for a public key.
-func (k *PublicKey) Address() string {
-	b := []byte(k.Hex())
-	h := hashing.Hash160(b)
+func Address(pubKey []byte) string {
+	h := hashing.Hash160(pubKey)
 	return base58.EncodeCheck(h, byte(base58.Address))
 }
 
-// Hex returns the hex encoding of the PublicKey.
-func (k *PublicKey) Hex() string {
-	bigZero := big.NewInt(0)
-	xb := k.X.Bytes()
-	var h string
-	if k.compressed {
-		h = hex.EncodeToString(xb)
-		if k.Y.Cmp(bigZero) == -1 {
-			h = "03" + h
-		} else {
-			h = "02" + h
-		}
+// A PublicKey wraps an ecdsa.PublicKey represents a Bitcoin public key.
+type PublicKey ecdsa.PublicKey
+
+// ToECDSA returns the public key as a *ecdsa.PublicKey.
+func (p *PublicKey) ToECDSA() *ecdsa.PublicKey {
+	return (*ecdsa.PublicKey)(p)
+}
+
+// IsEqual compares this PublicKey instance to the one passed, returning true if
+// both PublicKeys are equivalent. A PublicKey is equivalent to another, if they
+// both have the same X and Y coordinate.
+func (p *PublicKey) IsEqual(otherPubKey *PublicKey) bool {
+	return p.X.Cmp(otherPubKey.X) == 0 &&
+		p.Y.Cmp(otherPubKey.Y) == 0
+}
+
+// SerializeUncompressed serializes a public key in a 65-byte uncompressed
+// format.
+func (p *PublicKey) SerializeUncompressed() []byte {
+	b := make([]byte, 0, PubKeyBytesLenUncompressed)
+	b = append(b, pubkeyUncompressed)
+	b = paddedAppend(32, b, p.X.Bytes())
+	return paddedAppend(32, b, p.Y.Bytes())
+}
+
+// SerializeCompressed serializes a public key in a 33-byte compressed format.
+func (p *PublicKey) SerializeCompressed() []byte {
+	var prefix byte
+	if isOdd(p.Y) {
+		prefix = pubKeyCompressedOddY
 	} else {
-		b := append(xb, k.Y.Bytes()...)
-		h = "04" + string(b[:])
+		prefix = pubkeyCompressedEvenY
 	}
-	return h
+
+	b := make([]byte, 0, PubKeyBytesLenCompressed)
+	b = append(b, prefix)
+	return paddedAppend(32, b, p.X.Bytes())
+}
+
+// paddedAppend appends the src byte slice to dst, returning the new slice.
+// If the length of the source is smaller than the passed size, leading zero
+// bytes are appended to the dst slice before appending src.
+func paddedAppend(size uint, dst, src []byte) []byte {
+	for i := 0; i < int(size)-len(src); i++ {
+		dst = append(dst, 0)
+	}
+	return append(dst, src...)
 }
