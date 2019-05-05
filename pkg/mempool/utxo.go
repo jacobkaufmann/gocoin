ac8603656a36dedeee6e56f3eac6b2e7bbaff7fd
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-05-04 20:28:25 -0700

    Initial commit for UTxO.

---
+++ pkg/mempool/utxo.go
@@ -0,0 +1,7 @@
+package mempool
+
+// A UTxO represents an unspent transaction output.  Unspent transaction
+// outputs are the outputs of previous transactions which are eligible to
+// be consumed by future transactions.
+type UTxO struct {
+}
