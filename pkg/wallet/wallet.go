ddc287283960cb0a193d1801db6a3ae10453fbe5
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-05-04 20:28:34 -0700

    Initial commit for wallet.

---
+++ pkg/wallet/wallet.go
@@ -0,0 +1,11 @@
+package wallet
+
+// A Wallet represents a Bitcoin wallet.
+type Wallet struct {
+}
+
+// Balance returns the cumulative balance in satoshis for all
+// keys managed by the wallet.
+// func (w *Wallet) Balance() int64 {
+// TODO: Decide what implementation goes here and what goes in mempool pkg.
+// }
