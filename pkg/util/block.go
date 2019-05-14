33cc30a32ba655e57dd5f0ea458c7b506aeb5120
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-05-13 20:40:06 -0700

    Hide fields and build BlockHeader as Block view.

--- pkg/util/block.go
+++ pkg/util/block.go
@@ -1,7 +1,7 @@
 package util
 
 import (
-	"encoding/binary"
+	"math"
 
 	"github.com/jacobkaufmann/gocoin/pkg/crypto/hashing"
 )
@@ -9,69 +9,78 @@ import (
 // HeaderSize of array used to store serialized block header.
 const HeaderSize = 80
 
-// A Block represents a block in the Bitcoin block chain
+// A Block represents a block in the bitcoin block chain.
 type Block struct {
-	Size            uint32
-	Header          BlockHeader
-	NumTransactions uint32
-	Transactions    []*Tx
-}
-
-// A BlockHeader contains metadata for a block and is hashed as part
-// of Bitcoin's proof of work algorithm.
-type BlockHeader struct {
 	// The block version number.
-	Version uint32
+	version uint32
 
 	// The hash of the previous block's header.
-	PrevBlockHash hashing.Hash
+	prevBlockHash *hashing.Hash
 
 	// The hash of the merkle root.
-	MerkleRootHash hashing.Hash
+	merkleRootHash *hashing.Hash
 
 	// Unix epoch time when the miner started hashing the header
 	// (according to the miner).
-	Time uint32
+	time uint32
 
 	// An encoded version of the 256-bit target threshold this block's header
 	// hash must be less than or equal to.
-	NumBits uint32
+	numBits uint32
 
 	// An arbitrary number miners change to modify the header hash in order to
 	// produce a hash less than or equal to the target threshold.
 	// If all 32-bit values are tested, the time can be updated or the coinbase
 	// transaction can be changed and the merkle root updated.
-	Nonce uint32
+	nonce uint32
+
+	// Number of transaction entries.
+	txCount CompactSize
+
+	// The transactions included in the block.
+	txns []*Tx
 }
 
-// Serialize returns the contents of the block header encoded into a single byte
-// array.
-func (h *BlockHeader) Serialize() [HeaderSize]byte {
-	var b [HeaderSize]byte
-	binary.LittleEndian.PutUint32(b[:4], h.Version)
-	copy(b[4:36], h.PrevBlockHash.Bytes())
-	copy(b[36:68], h.MerkleRootHash.Bytes())
-	binary.LittleEndian.PutUint32(b[68:72], h.Time)
-	binary.LittleEndian.PutUint32(b[72:76], h.NumBits)
-	binary.LittleEndian.PutUint32(b[76:], h.Nonce)
-	return b
+// Header assembles and returns the block header corresponding to a block.
+func (blck *Block) Header() *BlockHeader {
+	return &BlockHeader{
+		version:        &blck.version,
+		prevBlockHash:  blck.prevBlockHash,
+		merkleRootHash: blck.merkleRootHash,
+		time:           &blck.time,
+		numBits:        &blck.numBits,
+		nonce:          &blck.nonce,
+		txCount:        &blck.txCount,
+	}
 }
 
-// Deserialize converts a serialized byte array into a block header object and
-// returns it.
-func Deserialize(b [HeaderSize]byte) (*BlockHeader, error) {
-	h := &BlockHeader{}
-	h.Version = binary.LittleEndian.Uint32(b[:4])
-	err := h.PrevBlockHash.SetBytes(b[4:36])
-	if err != nil {
-		return nil, err
+// maxNonce is a convenience variable representing the maximum possible nonce
+// value.
+const maxNonce = math.MaxUint32
+
+// IncrementNonce increments the block's nonce.  If the nonce is at its maximum
+// value, the nonce goes to zero and the reset flag is set to true to notify
+// the miner that all nonce values have been exhausted.
+//
+// Incrementing the nonce is fundamental to the mining process.
+func (blck *Block) IncrementNonce() (reset bool) {
+	if blck.nonce == maxNonce {
+		reset = true
 	}
-	err = h.MerkleRootHash.SetBytes(b[36:68])
-	if err != nil {
-		return nil, err
-	}
-	h.Time = binary.LittleEndian.Uint32(b[68:72])
-	h.NumBits = binary.LittleEndian.Uint32(b[72:76])
-	h.Nonce = binary.LittleEndian.Uint32(b[76:])
-	return h, nil
+	blck.nonce++
+	return
+}
+
+// A BlockHeader contains metadata for a block and is hashed as part
+// of bitcoin's proof of work algorithm.
+//
+// A BlockHeader functions as a view of a block.
+type BlockHeader struct {
+	version        *uint32
+	prevBlockHash  *hashing.Hash
+	merkleRootHash *hashing.Hash
+	time           *uint32
+	numBits        *uint32
+	nonce          *uint32
+	txCount        *CompactSize
 }
