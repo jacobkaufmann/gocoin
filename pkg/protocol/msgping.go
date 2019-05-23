3829c4f887f678f36fc0cc97e2221428b9eb636e
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-05-23 08:29:47 -0700

    Finish implementing Message interface.

--- pkg/protocol/msgping.go
+++ pkg/protocol/msgping.go
@@ -1,14 +1,53 @@
 package protocol
 
+import "io"
+
 // A MsgPing is sent to confirm the TCP/IP connection is still valid.
 type MsgPing struct {
 	nonce uint64
 }
 
+// NewMsgPing returns a new ping message containing a specified nonce.
+func NewMsgPing(nonce uint64) *MsgPing {
+	return &MsgPing{
+		nonce: nonce,
+	}
+}
+
+// Serialize converts the ping message into bytes as specified by the protocol
+// version pver and writes those bytes to w.
+func (msg *MsgPing) Serialize(w io.Writer, pver uint32) error {
+	buf := make([]byte, 8)
+	littleEndian.PutUint64(buf, msg.nonce)
+	_, err := w.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Deserialize reads from r and converts those bytes into a ping message as
+// specified by the protocol version pver.
+func (msg *MsgPing) Deserialize(r io.Reader, pver uint32) error {
+	buf := make([]byte, 8)
+	if _, err := io.ReadFull(r, buf); err != nil {
+		return err
+	}
+	msg.nonce = littleEndian.Uint64(buf)
+	return nil
+}
+
+// Command returns the message type of the ping message.
 func (msg *MsgPing) Command() MsgType {
 	return MsgTypePing
 }
 
+// MaxPayloadLength returns the maximum length in bytes of the ping message.
 func (msg *MsgPing) MaxPayloadLength(pver uint32) uint32 {
 	return 8
+}
+
+// Nonce returns the nonce of the ping message.
+func (msg *MsgPing) Nonce() uint64 {
+	return msg.nonce
 }
--- pkg/protocol/msgpong.go
+++ pkg/protocol/msgpong.go
@@ -1,5 +1,7 @@
 package protocol
 
+import "io"
+
 // A MsgPong is sent in response to a ping message.  In modern protocol
 // versions, a pong response is generated using a nonce included in the
 // corresponding ping.
@@ -7,10 +9,47 @@ type MsgPong struct {
 	nonce uint64
 }
 
+// NewMsgPong returns a new pong message containing a specified nonce.
+func NewMsgPong(nonce uint64) *MsgPong {
+	return &MsgPong{
+		nonce: nonce,
+	}
+}
+
+// Serialize converts the pong message into bytes as specified by the protocol
+// version pver and writes those bytes to w.
+func (msg *MsgPong) Serialize(w io.Writer, pver uint32) error {
+	buf := make([]byte, 8)
+	littleEndian.PutUint64(buf, msg.nonce)
+	_, err := w.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Deserialize reads from r and converts those bytes into a pong message as
+// specified by the protocol version pver.
+func (msg *MsgPong) Deserialize(r io.Reader, pver uint32) error {
+	buf := make([]byte, 8)
+	if _, err := io.ReadFull(r, buf); err != nil {
+		return err
+	}
+	msg.nonce = littleEndian.Uint64(buf)
+	return nil
+}
+
+// Command returns the message type of the pong message.
 func (msg *MsgPong) Command() MsgType {
 	return MsgTypePong
 }
 
+// MaxPayloadLength returns the maximum length in bytes of the pong message.
 func (msg *MsgPong) MaxPayloadLength(pver uint32) uint32 {
 	return 8
+}
+
+// Nonce returns the nonce of the pong message.
+func (msg *MsgPong) Nonce() uint64 {
+	return msg.nonce
 }
