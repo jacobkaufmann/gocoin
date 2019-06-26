c857f17550b5903b67b23cc4ac67fe854018cf90
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-06-25 20:32:09 -0700

    Implement serializable.

--- pkg/protocol/varstr.go
+++ pkg/protocol/varstr.go
@@ -1,7 +1,38 @@
 package protocol
 
+import "io"
+
 // A VarStr is a variable length string.
 type VarStr string
+
+// Serialize serializes vstr and writes to w.
+func (vstr *VarStr) Serialize(w io.Writer, pver uint32) error {
+	b := []byte(vstr)
+
+	err := vstr.Size().Serialize(w, pver)
+	if err != nil {
+		return err
+	}
+	return writeElement(w, b)
+}
+
+// Deserialize deserializes data from r into vstr.
+func (vstr *VarStr) Deserialize(r io.Reader, pver uint32) error {
+	var strLen CompactSize
+	err := strLen.Deserialize(r, pver)
+	if err != nil {
+		return err
+	}
+
+	var b [strLen]byte
+	err = readElement(r, b)
+	if err != nil {
+		return err
+	}
+
+	vstr = string(b)
+	return nil
+}
 
 // Size returns the length of the underlying string as a CompactSize.
 func (vstr VarStr) Size() CompactSize {
