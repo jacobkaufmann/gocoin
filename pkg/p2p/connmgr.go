fdf15f331a4910c9926c0e88c2a83dbab70ad038
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-07-21 13:39:02 -0700

    Modify conn map to hold peer objects and add methods to retrieve and update conns.

--- pkg/p2p/connmgr.go
+++ pkg/p2p/connmgr.go
@@ -1,14 +1,19 @@
 package p2p
 
-import "net"
+import (
+	"log"
+	"sync"
+
+	"github.com/jacobkaufmann/gocoin/pkg/protocol"
+)
 
 const (
 	// MaxOutboundConns is the maximum number of outgoing peer connections.
 	MaxOutboundConns = 8
 
-	// DefaultMaxPeerConns is the maximum number of peer connections to
+	// MaxInboundConns is the maximum number of incoming peer connections to
 	// maintain.
-	DefaultMaxPeerConns = 125
+	MaxInboundConns = 125
 
 	// PingInterval is the time between pings automatically sent out for
 	// latency probing and keepalive.
@@ -16,21 +21,96 @@ const (
 
 	// TimeoutInterval is the time after which to disconnect from inactivity.
 	TimeoutInterval = 20 * 60
+
+	// MaxRetries is the number of retries before no more attempts are made to
+	// connect to a peer.
+	MaxRetries = 3
 )
 
 // ConnManager manages the connections of a Bitcoin node.
 type ConnManager struct {
-	Conns map[string]*net.TCPConn
+	Conns       map[string]*Peer
+	muxConns    sync.RWMutex
+	NumOutbound int
+	NumInbound  int
 }
 
 // NewConnManager creates a new connection manager.
 func NewConnManager() *ConnManager {
 	return &ConnManager{
-		Conns: make(map[string]*net.TCPConn),
+		Conns: make(map[string]*Peer),
 	}
 }
 
 // NumConns returns the number of connections under management of mgr.
 func (mgr *ConnManager) NumConns() int {
 	return len(mgr.Conns)
+}
+
+// GetConn retrieves a peer connection to addr from mgr's connection pool if
+// such a connection exists.
+func (mgr *ConnManager) GetConn(addr string) *Peer {
+	mgr.muxConns.RLock()
+	defer mgr.muxConns.RUnlock()
+	p, ok := mgr.Conns[addr]
+	if ok {
+		return p
+	}
+	return nil
+}
+
+// RemoveConn removes the connection to addr from mgr's connection pool if
+// such a connection exists.
+func (mgr *ConnManager) RemoveConn(addr string) (removed bool) {
+	mgr.muxConns.Lock()
+	defer mgr.muxConns.Unlock()
+
+	p, ok := mgr.Conns[addr]
+	if !ok {
+		return false
+	}
+
+	if p.Inbound {
+		mgr.NumInbound--
+	} else {
+		mgr.NumOutbound--
+	}
+
+	delete(mgr.Conns, addr)
+	return true
+}
+
+// AddConn attempts to add a connection with p to mgr's connection pool.
+func (mgr *ConnManager) AddConn(p *Peer) (added bool) {
+	mgr.muxConns.Lock()
+	defer mgr.muxConns.Unlock()
+	addr := p.Conn.RemoteAddr().String()
+
+	if p.Inbound {
+		if mgr.NumInbound < MaxInboundConns {
+			mgr.Conns[addr] = p
+			mgr.NumInbound++
+			added = true
+		} else {
+			log.Printf("unable to establish inbound conn with %s", addr)
+		}
+	} else {
+		if mgr.NumOutbound < MaxOutboundConns {
+			mgr.Conns[addr] = p
+			mgr.NumOutbound++
+			added = true
+		} else {
+			log.Printf("unable to establish outbound conn with %s", addr)
+		}
+	}
+
+	return added
+}
+
+// SetServices sets the service flags for the peer at addr to svc.
+func (mgr *ConnManager) SetServices(addr string, svc protocol.ServiceFlag) {
+	p := mgr.GetConn(addr)
+	if p != nil {
+		p.Services = svc
+	}
 }
