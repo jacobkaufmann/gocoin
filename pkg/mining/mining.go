d70149eeb8490db765d13d78994d9bdb1878eabb
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-08-08 21:45:09 -0700

    Miner interface.

--- pkg/mining/mining.go
+++ pkg/mining/mining.go
@@ -2,6 +2,7 @@ package mining
 
 import (
 	"github.com/jacobkaufmann/gocoin/pkg/crypto/hashing"
+	"github.com/jacobkaufmann/gocoin/pkg/util"
 )
 
 const (
@@ -12,3 +13,8 @@ const (
 var (
 	coinbasePrevTxHash = hashing.Hash([hashing.HashSize]byte{0})
 )
+
+// Miner represents a bitcoin mining node.
+type Miner interface {
+	Mine(blk *util.Block)
+}
